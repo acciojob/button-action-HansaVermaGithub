265aaccabd2c0165a29d9491f8ca88bd7af3d8a5
unknown <hansaverma88@gmail.com>
2026-03-28 23:04:16 +0530

    third commit

--- src/components/ToggleParagraph.js
+++ src/components/ToggleParagraph.js
@@ -1,18 +1,18 @@
 import React, { useState } from 'react';
 
 function ToggleParagraph(){
-const [isVisible, setIsVisible] = useState(false);
 
-const handleClick = () => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  const handleClick = () => {
     setIsVisible(!isVisible);
 }
-  return(
+   return(
     <div id="main">
-        <button id='click' onClick={handleClick}>
+        <button id="click"  onClick={handleClick}>
           {isVisible ?  'Hide Paragraph' : 'Show Paragraph' }
-          
         </button>
-        {isVisible && <p id='para'>"Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy".</p>}
+        {isVisible && (<p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy.</p>)}
     </div>
   )
 }
